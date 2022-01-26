63bed3fa6317e2c6a1995e98a1aa2c53fe485b7b
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-26 22:56:56 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/durationapi/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.metehanbolat.durationapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metehanbolat.durationapi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+}
